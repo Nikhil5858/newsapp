2c430580be757a43ec1dacabb3798d8772ed5b4b
Nikhil5858 <123rathodnikhil@gmail.com>
2024-04-07 16:58:12 +0530

    News App 👍

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default class App extends Component {
   pageSize = 6;
+  apiKey = process.env.REACT_APP_NEWS_API
   render() {
     return (
       <div>
       <Router>
         <Navbar/>
           <Routes>
-            <Route exact path="/general" element={<News key={"general"} pageSize={this.pageSize} country={"in"} category={"general"} />} />
-            <Route exact path="/business" element={<News key={"business"} pageSize={this.pageSize} country={"in"} category={"business"} />} />
-            <Route exact path="/entertainment" element={<News key={"entertainment"} pageSize={this.pageSize} country={"in"} category={"entertainment"} />} />
-            <Route exact path="/health" element={<News key={"health"} pageSize={this.pageSize} country={"in"} category={"health"} />} />
-            <Route exact path="/technology" element={<News key={"technology"} pageSize={this.pageSize} country={"in"} category={"technology"} />} />
-            <Route exact path="/science" element={<News key={"science"} pageSize={this.pageSize} country={"in"} category={"science"} />} />
-            <Route exact path="/sports" element={<News key={"sports"} pageSize={this.pageSize} country={"in"} category={"sports"} />} />
+          <Route exact path="/general" element={<News key={"general"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"general"} />} />
+          <Route exact path="/business" element={<News key={"business"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"business"} />} />
+          <Route exact path="/entertainment" element={<News key={"entertainment"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"entertainment"} />} />
+          <Route exact path="/health" element={<News key={"health"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"health"} />} />
+          <Route exact path="/technology" element={<News key={"technology"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"technology"} />} />
+          <Route exact path="/science" element={<News key={"science"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"science"} />} />
+          <Route exact path="/sports" element={<News key={"sports"} apiKey={this.apiKey} pageSize={this.pageSize} country={"in"} category={"sports"} />} />
+
           </Routes>
         <Footer/>
       </Router>
